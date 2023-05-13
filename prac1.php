1ec97b4dcf53873e842c437021cf0a9c24a564e2
jerry parrocha <j.parrocha@mlgcl.edu.ph>
2023-05-13 14:14:32 +0800

    update

--- prac1.php
+++ prac1.php
@@ -1,5 +1,10 @@
 <?php
 
+interface Table 
+{
+    public function createtbl();
+}
+
 abstract class Database
 {
     public $conn;
@@ -14,14 +19,28 @@ abstract class Database
     }
     abstract public function db(): string;
 }
-class Dbname extends Database
+class Dbname extends Database implements Table
 {
     public function db(): string
     {
         $connect = "CREATE DATABASE IF NOT EXISTS $this->dbname";
         return $this->conn->query($connect);
     }
+    public function createtbl()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS person(
+            id int auto_increment primary key,
+            first_name text,
+            last_name text,
+            middle_name text,
+            age int
+            )";
+        $this->conn->query($sql);    
+    }
 }
+
 $test = new Dbname();
 $test->db();
+var_dump($test->createtbl());
+
 ?>
