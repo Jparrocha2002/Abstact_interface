f61bee24835854a62ede809e18e03ec2840e3e7f
jerry parrocha <j.parrocha@mlgcl.edu.ph>
2023-05-13 11:34:56 +0800

    update

--- prac1.php
+++ prac1.php
@@ -5,7 +5,13 @@ abstract class Database
     private $conn;
     public $dbname = "parrocha";
 
-    
+    public function connect()
+    {
+        $this->conn = new mysqli("localhost","root","");
+        $this->conn->query("CREATE DATABASE IF NOT EXISTS $this->dbname");
+        $this->conn = new mysqli("localhost","root","", $this->dbname);
+    }
 }
-
+$connect = new Database();
+$connect->connect();
 ?>
